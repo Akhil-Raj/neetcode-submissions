0b83a6b72342604ac532a22fb88c609c102fde7e
Akhil Raj <akhil.raj1997@gmail.com>
2026-09-14 18:20:58 +0530

    Add: coin-change-ii - submission-4

---
+++ Data Structures & Algorithms/coin-change-ii/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def change(self, target: int, coins: List[int]) -> int:
+        noc = [[-1 for mdi in range(len(coins))] for t in range(target + 1)]
+        # coins.sort()
+
+        def find_noc(target, mdi):
+            if target == 0:
+                return 1
+            if mdi < 0 or target < 0:
+                return 0
+            if noc[target][mdi] != -1:
+                return noc[target][mdi]
+            
+            noc[target][mdi] = 0
+            # if taking mdi's val, and can take it again
+            if target >= coins[mdi]:
+                noc[target][mdi] += find_noc(target - coins[mdi], mdi)
+            # if not taking mdi's val
+            noc[target][mdi] += find_noc(target, mdi - 1)
+            
+            return noc[target][mdi]
+        
+        noc[target][len(coins) -1] = find_noc(target, len(coins) - 1)
+        # print(noc)
+        return noc[target][len(coins) - 1]
